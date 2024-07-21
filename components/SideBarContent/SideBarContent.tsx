1b56af550cfe754d9bab29ca316c1ae3d9efe397
Jerome <jeromexlee@gmail.com>
2024-07-21 11:30:26 -0700

    add

--- components/SideBarContent/SideBarContent.tsx
+++ components/SideBarContent/SideBarContent.tsx
@@ -3,6 +3,7 @@ import { NumberInput } from '@mantine/core';
 import { useState } from 'react';
 import { Button, Switch } from '@mantine/core';
 import { IconDownload, IconActivityHeartbeat } from '@tabler/icons-react';
+import axios from 'axios';
 
 
 
@@ -12,6 +13,36 @@ export function SideBardContent() {
   const [weightValue, setWeightValue] = useState<string | number>('');
   const [heightValue, setHeightValue] = useState<string | number>('');
   const [isLiveData, { toggle }] = useDisclosure();
+
+  const handleDownload = async () => {
+    try {
+      // Replace this with your actual API endpoint
+      const response = await axios.get('http://100.28.74.221:5000/read', {
+        params: {
+          age: ageValue,
+          weight: weightValue,
+          height: heightValue,
+          liveData: isLiveData,
+        }
+      });
+
+    //   if (response.data && response.data.url) {
+    //     const link = document.createElement('a');
+    //     link.href = response.data.url;
+    //     link.download = 'file.pdf'; // Adjust the file name and extension as needed
+    //     document.body.appendChild(link);
+    //     link.click();
+    //     document.body.removeChild(link);
+    //   } else {
+    //     console.error('URL not found in response payload');
+    //   }
+      console.log("XCXC")
+      console.log(response)
+    } catch (error) {
+        console.log("XCXC: ERROR")
+      console.error('Error downloading file:', error);
+    }
+  };
 
 
   return (
@@ -23,7 +54,7 @@ export function SideBardContent() {
         <Button variant="filled" onClick={() => console.log("Clicked")}>Update</Button>
         <Switch checked={isLiveData} onChange={toggle} label="If fetch real time heartbeat?" mt="md" />
         <p></p>
-        <Button rightSection={<IconDownload size={14} />}  onClick={() => console.log("Clicked")}>Download</Button>
+        <Button rightSection={<IconDownload size={14} />} onClick={handleDownload}>Download</Button>
     </>
   );
 }
